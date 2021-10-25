877e598f2626fe4404b808015498924bc6fa43b5
Andrew <andrewborg86@gmail.com>
2021-10-26 00:18:44 +1100

    committing header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import styles from "./Header.module.scss"
+import cloudArt from "../../../src/assets/images/cloudArt.png"
 
 import TheLooney from '../TheLooney/TheLooney';
 const Header = () => {
   return(
     <div className={styles.header}>
-      <h1>LooNeY LoLLieS</h1>
+      <img className={styles.Image} src={cloudArt} alt=""/>
+      <h1 className={styles.Title}>LoONeY LoLLieS</h1>
 
       <TheLooney />
 
