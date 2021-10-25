548194282617fd6df843ed99aba6482e0dad9b70
Andrew <andrewborg86@gmail.com>
2021-10-25 22:53:24 +1100

    removed underlines from card text. adjust card title position. limited description wrap lines.

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           console.log(item);
           return (
             <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={styles.productGridItem}>
-              <Link to={'/product/' + item.id}>
+              <Link className={styles.Link} to={'/product/' + item.id}>
                 <ProductCard md="4" key={item.id} product={item}/>
               </Link>
             </Grid>
